0c143e373fe060d00cdd9dc14b8da4972be06b5d
sarotpum <sr.pummanee@gmail.com>
2022-07-13 23:38:36 +0700

    add project part2 demo11-12

---
+++ part2_demo11_serve_static_files/index.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/image", func (c *gin.Context)  {
+		c.File("static/test.jpg")
+	})
+	
+	r.GET("/html", func (c *gin.Context)  {
+		c.File("static/index.html")
+	})
+
+	r.GET("/download", func (c *gin.Context)  {
+		c.Header("Content-Description", "Simulation File Download")
+		c.Header("Content-Transfer-Encoding", "binary")
+		c.Header("Content-Disposition", "attachment; filename="+"download.jpg")
+		c.Header("Content-Type", "application/octet-stream")
+
+		c.File("static/test.jpg")
+	})
+
+	r.Run(":85")
+}
